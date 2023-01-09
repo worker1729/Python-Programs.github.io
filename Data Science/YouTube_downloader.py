ff342a8e4adac9549e86725575bb949729423d2e
Bruce Lopez <112580907+worker1729@users.noreply.github.com>
2023-01-08 21:45:20 -0600

    Create YouTube_downloader.py

---
+++ Data Science/YouTube_downloader.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+
+def Download(link):
+  youtubeObject = YouTube(link)
+  youtubeObject = youtubeObject.streams.get_highest_resolution()
+  try:
+      youtubeObject.download()
+  except:
+    print("There has been an error in downloading your youtube video")
+  print("This download has completed! Yahooooo!")
+
+link = input("Put your youtube link here!! URL: ")
+Download(link)
