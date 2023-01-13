cf2fa3152ab2e4b85bac45f330b58947f1b6f27c
Bruce Lopez <112580907+worker1729@users.noreply.github.com>
2023-01-12 22:33:48 -0600

    Add files via upload

--- Data Science/Web Scraping/scraping.py
+++ Data Science/Web Scraping/scraping.py
@@ -4,6 +4,12 @@ with open("index.html", "r") as html_file:
     content = html_file.read()
 
     soup = BeautifulSoup(content, 'lxml')
-    courses_html_tags = soup.find_all('h2')
-    for course in courses_html_tags:
-        print(course.text)
+    course_cards = soup.find_all('div', class_="button")
+    for x in course_cards:
+        course_name = x.h2.text
+        course_price = x.button.text.split()[-1]
+        print(f'{course_name} costs {course_price}')
+        #print(course_price)
+    #courses_html_tags = soup.find_all('h2')
+    #for course in courses_html_tags:
+        #print(course.text)
